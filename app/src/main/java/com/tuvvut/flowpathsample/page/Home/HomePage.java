14c7641f8173c1a8272a9f2ad8b1897c4b75049d
wu <tuvvut@gmail.com>
2015-10-05 10:53:42 +0800

    change onPrepareOptionsMenu event to onCreateOptionsMenu

--- app/src/main/java/com/tuvvut/flowpathsample/page/Home/HomePage.java
+++ app/src/main/java/com/tuvvut/flowpathsample/page/Home/HomePage.java
@@ -45,7 +45,7 @@ public class HomePage extends RelativeLayout implements PageListener {
     }
 
     @Override
-    public void onPrepareOptionsMenu(Activity activity, Menu menu) {
+    public void onCreateOptionsMenu(Activity activity, Menu menu) {
         activity.setTitle(this.getClass().getSimpleName());
         activity.getMenuInflater().inflate(R.menu.page_home_menu, menu);
 
